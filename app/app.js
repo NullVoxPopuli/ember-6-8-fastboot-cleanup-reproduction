d7791ca086dd1f7bd20c05d226005bc5033c3175
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-06 12:58:29 -0500

    Simplify repro

--- app/app.js
+++ app/app.js
@@ -1,13 +1,7 @@
-import '@warp-drive/ember/install';
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-6-8-fastboot-cleanup-reproduction/config/environment';
-import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
-
-if (macroCondition(isDevelopingApp())) {
-  importSync('./deprecation-workflow');
-}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
